6553180b650f9fbe348ef10c938ad5da22eb94d7
LenaIT <olena.denysova@gmail.com>
2020-08-13 23:31:18 +0200

    manuel working

--- src/index.jsx
+++ src/index.jsx
@@ -3,31 +3,28 @@ import React from "react";
 import reactDOM from "react-dom";
 import "./Img.css";
 
-function ImgItem ({src, alt, className}) {
+function ImgItem({ src, alt, size, type }) {
   return (
-   
-      <div className="avatar">
-        <img src={src}  
-              alt={alt}
-              className={className} />
-        <p className="size">
-          Size <span></span> with{" "}
-        </p>
-        <p className="type">
-          Type <span></span>
-        </p>
-      </div>
-   
-  )
+    <div className="avatar">
+      <img src={src} alt={alt} className={`"${size} ${type}"`} />
+      <p className="size">
+        Size <span>{size}</span> with{" "}
+      </p>
+      <p className="type">
+        Type <span>{type}</span>
+      </p>
+    </div>
+  );
 }
-
 
 const All = () => {
   return (
     <div className="pictures">
-      <ImgItem />
+      <ImgItem src="./img/dog.jpg" alt="avatar" size="s" type="circle" />
+      <ImgItem src="./img/dog.jpg" alt="avatar" size="s" type="square" />
+      <ImgItem src="./img/dog.jpg" alt="avatar" size="s" type="rounded" />
     </div>
   );
 };
 
-reactDOM.render(<ImgItem src="./img/dog.jpg" alt="avatar" className="s circle"/>, document.getElementById("root"));
+reactDOM.render(<All />, document.getElementById("root"));
